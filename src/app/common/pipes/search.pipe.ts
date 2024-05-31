21d03af65c1722af9b98e31e515667b8e0a74ea5
Ayush2395 <ak005355@gmail.com>
2024-05-31 22:38:37 +0530

    minor changes

--- src/app/common/pipes/search.pipe.ts
+++ src/app/common/pipes/search.pipe.ts
@@ -7,8 +7,8 @@ import { IProducts } from '../models/model';
 export class SearchPipe implements PipeTransform {
 
   transform(value: IProducts[], searchValue: string): IProducts[] {
-    return value.filter(item => item.category.name.toLowerCase().includes(searchValue)
-      || item.title.toLowerCase().includes(searchValue));
+    return value.filter(item => item.category.name.toLowerCase().includes(searchValue.toLowerCase())
+      || item.title.toLowerCase().includes(searchValue.toLowerCase()));
   }
 
 }
